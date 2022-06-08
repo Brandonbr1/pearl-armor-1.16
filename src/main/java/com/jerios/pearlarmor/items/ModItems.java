90a4aecbd31fff2df7d4ef8c59658f2d2e4d0fb7
Java sauce <mohammedbrandon204@gmail.com>
2022-06-08 13:39:56 -0700

    try to add armor

--- src/main/java/com/jerios/pearlarmor/items/ModItems.java
+++ src/main/java/com/jerios/pearlarmor/items/ModItems.java
@@ -1,8 +1,8 @@
 package com.jerios.pearlarmor.items;
 
+
 import com.jerios.pearlarmor.PearlArmor;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.*;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,6 +14,28 @@ public class ModItems {
 
         public static final DeferredRegister<Item> ITEMS =
                 DeferredRegister.create(ForgeRegistries.ITEMS, PearlArmor.MOD_ID);
+
+        public static final RegistryObject<Item> AMETHYST_SWORD = ITEMS.register("amethyst_sword",
+                () -> new SwordItem(ModItemTier.AMETHYST, 2, 3f,
+                        new Item.Properties()));
+
+        public static final RegistryObject<Item> AMETHYST_PICKAXE = ITEMS.register("amethyst_pickaxe",
+                () -> new PickaxeItem(ModItemTier.AMETHYST, 0, -1f,
+                        new Item.Properties()));
+
+        public static final RegistryObject<Item> AMETHYST_SHOVEL = ITEMS.register("amethyst_shovel",
+                () -> new ShovelItem(ModItemTier.AMETHYST, 0, -1f,
+                        new Item.Properties()));
+
+        public static final RegistryObject<Item> AMETHYST_AXE = ITEMS.register("amethyst_axe",
+                () -> new AxeItem(ModItemTier.AMETHYST, 4, -6f,
+                        new Item.Properties()));
+
+        public static final RegistryObject<Item> AMETHYST_HOE = ITEMS.register("amethyst_hoe",
+                () -> new HoeItem(ModItemTier.AMETHYST, 0, 0f,
+                        new Item.Properties()));
+
+
 
         public static final RegistryObject<Item> PEARLORB = ITEMS.register("pearlorb",
                 () -> new Item(new Item.Properties()));
